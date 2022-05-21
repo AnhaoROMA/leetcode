6f7d0d6427a4a1967b312a278f8ac6b2416c7e58
anhao <anhaoroma@163.com>
2022-05-21 22:12:45 +0800

    tarjan算法（须回看）

---
+++ Graph/1192 critical connections in a network.py
@@ -0,0 +1,26 @@
+"""
+https://leetcode.com/problems/critical-connections-in-a-network/
+"""
+
+# solution:
+# 求割点与桥的问题：Tarjan算法
+
+
+class Solution:
+    def criticalConnections(self, n, connections):
+        res, low, g = [], [None] * n, collections.defaultdict(list)
+        for i, j in connections:
+            g[i].append(j)
+            g[j].append(i)
+
+        def dfs(cur, v=0, p=None):
+            dfn = low[v] = cur
+            for i in g[v]:
+                if i != p:
+                    if low[i] == None:
+                        cur += 1
+                        dfs(cur, i, v)
+                        low[i] > dfn and res.append([v, i])
+                    low[v] = min(low[v], low[i])
+        dfs(0)
+        return res
