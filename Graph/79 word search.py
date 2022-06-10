0dda8ba9ede3112ed5bb87f0183888d8cb738d05
anhao <anhaoroma@163.com>
2022-06-10 10:07:05 +0800

    DFS+递归

--- Graph/79 word search.py
+++ Graph/79 word search.py
@@ -20,46 +20,75 @@ true
 """
 
 
-def word_search(board: list[list[int]], word: str) -> bool:
-    m = len(board)
-    n = len(board[0])
-    word_length = len(word)
-    directions = [
-        (1, 0),
-        (-1, 0),
-        (0, 1),
-        (0, -1)
-    ]
+# def word_search(board: list[list[int]], word: str) -> bool:
+#     m = len(board)
+#     n = len(board[0])
+#     word_length = len(word)
+#     directions = [
+#         (1, 0),
+#         (-1, 0),
+#         (0, 1),
+#         (0, -1)
+#     ]
+#
+#     for i in range(m):
+#         for j in range(n):
+#             if board[i][j] != word[0]:
+#                 continue
+#             else:
+#                 dfs = [
+#                     [
+#                         [(i, j)], (i, j), 1
+#                     ]
+#                 ]
+#                 while dfs:
+#                     visited, position, matched = dfs.pop()
+#                     if matched == word_length:
+#                         return True
+#                     for opt in directions:
+#                         x = position[0] + opt[0]
+#                         y = position[1] + opt[1]
+#                         if x < 0 or y < 0 or x >= m or y >= n or (x, y) in visited:
+#                             continue
+#                         if board[x][y] != word[matched]:
+#                             continue
+#                         else:
+#                             dfs.append(
+#                                 [
+#                                     visited + [(x, y)], (x, y), matched + 1
+#                                 ]
+#                             )
+#
+#     return False
 
-    for i in range(m):
-        for j in range(n):
-            if board[i][j] != word[0]:
-                continue
-            else:
-                dfs = [
-                    [
-                        [(i, j)], (i, j), 1
-                    ]
-                ]
-                while dfs:
-                    visited, position, matched = dfs.pop()
-                    if matched == word_length:
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        def dfs(x, y, p):
+            if p == l:
+                return True
+            for dirx, diry in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                curx, cury = x + dirx, y + diry
+                if 0 <= curx < m and 0 <= cury < n and \
+                        flag[curx][cury] and board[curx][cury] == word[p]:
+                    flag[curx][cury] = False
+                    if dfs(curx, cury, p + 1):
+                        flag[curx][cury] = True
                         return True
-                    for opt in directions:
-                        x = position[0] + opt[0]
-                        y = position[1] + opt[1]
-                        if x < 0 or y < 0 or x >= m or y >= n or (x, y) in visited:
-                            continue
-                        if board[x][y] != word[matched]:
-                            continue
-                        else:
-                            dfs.append(
-                                [
-                                    visited + [(x, y)], (x, y), matched + 1
-                                ]
-                            )
+                    flag[curx][cury] = True
+            return False
 
-    return False
+        m, n = len(board), len(board[0])
+        l = len(word)
+        flag = [[True] * n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == word[0]:
+                    flag[i][j] = False
+                    if dfs(i, j, 1):
+                        return True
+                    flag[i][j] = True
+        return False
 
 
 a = [
@@ -79,3 +108,9 @@ b = [
     ["A", "A", "A", "A", "B", "A"]
 ]
 print(word_search(b, "AAAAAAAAAAAAABB"))
+c = [
+    ["C", "A", "A"],
+    ["A", "A", "A"],
+    ["B", "C", "D"]
+]
+print(word_search(c, "AAB"))
