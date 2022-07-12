ecb4b40a3d0b20490698fbf48f52f9443a5c286f
anhao <anhaoroma@163.com>
2022-07-12 16:37:36 +0800

    回溯法（version-1）

---
+++ BackTracking/473 matchsticks to square.py
@@ -0,0 +1,54 @@
+"""
+https://leetcode.com/problems/matchsticks-to-square/
+
+You are given an integer array matchsticks
+    where matchsticks[i] is the length of the ith matchstick.
+You want to use all the matchsticks to make one square.
+You should not break any stick,
+but you can link them up,
+and each matchstick must be used exactly one time.
+
+Return true if you can make this square and false otherwise.
+
+Input: matchsticks = [1,1,2,2,2]
+Output: true
+
+Input: matchsticks = [3,3,3,3,4]
+Output: false
+"""
+
+
+def backtracking(choices: list[int], target: int, start: int) -> bool:
+    """
+    判断能否从 choices 这个数组中，找到和为 target 的子数组。
+    """
+    if target == 0:
+        return True
+    length = len(choices)
+    if start == length:
+        return False
+    for i in range(start, length):
+        temp = choices[i]
+        if temp > target:
+            continue
+        if not backtracking(choices[:i]+choices[i+1:], target-temp, i):
+            choices = choices[:i] + [temp] + choices[i+1:]
+        else:
+            choices.pop(i)
+            return True
+
+
+def make_square(matchsticks: list[int]) -> bool:
+    if sum(matchsticks) % 4 != 0:
+        return False
+    matchsticks.sort(reverse=True)
+    target = sum(matchsticks) // 4
+    for _ in range(4):
+        if not backtracking(matchsticks, target, 0):
+            return False
+    return True
+
+
+print(make_square([1, 1, 2, 2, 2]))
+print(make_square([5, 4, 3, 3, 2, 2, 1]))
+print(make_square([2, 2, 2, 2, 2, 6]))
